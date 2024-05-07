cd6cfb9cd2de50f60f58700d25f45f308f65b579
JeffKippersPXL <43740630+JeffKippersPXL@users.noreply.github.com>
2024-05-07 15:21:28 +0200

    Error gefixt waardoor de backend en front end niet met elkaar konden praten.
    Cross server talk is nu enabeld

--- app.js
+++ app.js
@@ -20,6 +20,15 @@ const port = 10000
 const bodyParser = require('body-parser')
 const urlendcodedParser = bodyParser.urlencoded({ extended: false })
 
+//enabeling cross server talk
+//CORS ook wel bekend als
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 //Supabase voor server
 //Resend voor mails
 
