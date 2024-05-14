f114d68f165d45b2f27bc3b184380a6b23de6836
JeffKippersPXL <43740630+JeffKippersPXL@users.noreply.github.com>
2024-05-14 16:47:54 +0200

    API Passwords aangepast dat het meer secure is, dit is nog WIP
    Nieuwe API gemaakt voor Events te kunnen handelen zodat deze dynamisch kunnen worden ingeladen op de site en op de database kunnen worden bijgehouden. Deze moeten nog gestest worden

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const resend = new Resend('re_D7KwAsKt_Q5hGWsQLgofjYdyY7CWebHNG');
 const express = require('express');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const secretKey = 'your-secret-key';//Edit to something more random and secure
 const app = express()
 
 //const port = 3000
@@ -20,6 +22,8 @@ const port = 10000
 
 //Used to be able to read body data
 const bodyParser = require('body-parser')
+const {hash} = require("crypto.js");
+const {response} = require("express");
 const urlendcodedParser = bodyParser.urlencoded({ extended: false })
 
 //enabeling cross server talk
@@ -104,10 +108,6 @@ app.post('/api/subscriptions',urlendcodedParser, async (req, res) =>
                     });
             }
             });
-
-
-
-
 });
 
 app.get('/api/subscriptionsConfirm', (req, res) => {
@@ -133,13 +133,82 @@ app.get('/api/subscriptionsConfirm', (req, res) => {
         });
 });
 
+app.post('api/subscriptionsDelete', (req, res) => {
+    const {email} = req.query;
+    supabase
+        .from('subscriptions')
+        .select('token')
+        .eq('email', email)
+        .then( response => {
+                if(response.status >=200 && response.status < 300){
+                    const {token} = response.data;
+                    resend.emails.send({
+                        from: 'onboarding@resend.dev',
+                        to: `${email}`,
+                        subject: 'Email verification',
+                        html: `<p>Click the following link to Delete your Subscription <strong><a href="http://localhost:10000/api/subscriptionsDelete?token=${token}">Confirm</strong>!</a></p>`
+                    });
+                    }
+            })
+        .catch(error => {
+            res.status(500).json({
+                message: 'error reading from database: ' + error.message
+            });
+        });
+});
+
+    //Delete
+app.get('api/subscriptionsDelete', (req,res) =>{
+    const{token} = req.query;
+    supabase
+        .from('subscriptions')
+        .delete()
+        .eq('token', token)
+        .then(response =>{
+            res.status(200).json({message:'U ben uitgeschreven voor de subscriber mail'});
+        })
+        .catch(
+            res.status(500).json({message:'Er is iets foutgelopen met u uit de lijst te halen, gelieven contact op te nemen met een admin'})
+        )
+    });
+
 //===================-API"s for Users & Passwords-=======================
 //
+app.post('/api/create-user', (req, res)=>{
+    const {email, name, password, } = req.body;
+    const salt = crypto.randomBytes(16).toString('hex');
+    let hashedPassword = crypto.pbkdf2Sync(password, salt, 1000,64, 'sha256').toString('hex');
+    //checks if the email exist in the database
+    supabase
+        .from("users")
+        .select('email')
+        .eq('email', email)
+        .then(response =>{
+                if(!response.code(200)){
+                    supabase
+                        .from('users')
+                        .insert({'email': email, 'password': hashedPassword, 'salt': salt})
+                        .then(
+                            response => {
+                                token = jwt.sign({token: token}, secret);
+                                resend.emails.send({
+                                    from: 'onboarding@resend.dev',
+                                    to: `${email}`,
+                                    subject: 'Email verification',
+                                    html: `<p>Click the following link to confirm your account <strong><a href="http://localhost:10000/api/forgot-password?token=${token}">Confirm</strong>!</a></p>`
+                                });
+                                res.status(200).json(response.data);
+                            })
+                }
+            }
+        )
+});
 //API to get a user based on email and password
 //Currently returns ranking(true or false) and a ID
 app.get('/api/Users', async (req, res)=>{
     let passwordHashed = req.query.password;
     let mail = req.query.email;
+    passwordHashed = crypto.randomUUID(passwordHashed);
     supabase
         .from('Users')
         .select('ranking, id')
@@ -147,7 +216,8 @@ app.get('/api/Users', async (req, res)=>{
         .eq('password', passwordHashed)
         .eq('email', mail)
         .then( response => {
-                res.status(200).json(response.data)
+            let token = jwt.sign({userId: response.data.id , isAdmin: response.data.ranking}, secretKey, {expiresIn: '1h'});
+                res.status(200).json(token)
             }
         )
         .catch( error =>
@@ -163,6 +233,8 @@ app.put('/api/Users', async (req, res) =>{
     let hashedPassword = req.query.password;
     let email = req.query.email;
     let hashedOldPassword = req.query.oldpassword;
+    hashedPassword = crypto.randomUUID(hashedOldPassword);
+    hashedOldPassword = crypto.randomUUID(hashedOldPassword);
     supabase
         .from('Users')
         .update({password: hashedPassword})
@@ -203,13 +275,119 @@ app.put('/api/UsersEmail', async (req, res) =>{
 });
 
 //Route to inital password reset WIP
-app.post('forgot-password', (req, res)=>{
+app.post('/api/forgot-password', (req, res)=>{
    const {email} = req.body;
    //checks if the email exist in the database
     supabase
         .from("users")
         .select('email')
         .eq('email', email)
+        .then(response =>{
+            if(response.code(200)){
+                supabase
+                    .from('users')
+                    .select('token')
+                    .eq('email', email)
+                    .then(
+                        response => {
+                            resend.emails.send({
+                                from: 'onboarding@resend.dev',
+                                to: `${email}`,
+                                subject: 'Email verification',
+                                html: `<p>Click the following link to confirm your account <strong><a href="http://localhost:10000/api/?token=${token}">Confirm</strong>!</a></p>`
+                            });
+
+                            res.status(200).json(response.data);
+                        }
+                    )
+            } else{
+                res.status(500).json("Error, email niet gevonden in de database");
+            }
+            }
+        )
+});
+
+app.get('/api/password-reset', async (req, res) =>{
+    let {token, password} = req.query;
+    //Use JWT for teh transfering of the token
+    supabase
+        .from('users')
+        .update()
+})
+
+//===================-API"s for Events and the Kalender-=======================
+//
+//
+app.post('/api/create-event', (req, res)=> {
+    const {name, description, starttime, date, endtime} = req.body;
+    supabase
+        .from('users')
+        .select('name')
+        .eq('ranking', true)
+        .then(response => {
+            if (response.code(200)) {
+                supabase
+                    .from("events")
+                    .insert(
+                        //generate unique token en voeg deze toe voro verificatie.
+                        {name: name,
+                            description: description,
+                            startTime: starttime,
+                            date: date,
+                            endtime: endtime
+                        }
+                    )
+                response.status(200).json({message: "Created event"});
+            } else {
+                response.status(500).json({message: "failed to create event"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: 'Error creating event'})
+        });
+
+});
+
+app.put('/api/edit-event', async (req, res) =>{
+    let name = req.query.name;
+    let description = req.query.description;
+    let starttime = req.query.startTime;
+    let date = req.query.date;
+    let endtime = req.query.endtime;
+    supabase
+        .from('events')
+        .update({name: name, description: description, startTime: starttime, date: date, endtime: endtime})
+        .eq('name', name)
+        .eq('password', hashedPassword)
+        .then( response => {
+                res.status(200).json({message: "Email is veranderd"})
+            }
+        )
+        .catch( error =>
+        {
+            res.status(500).json({
+                message: 'Error reading from Database: ' +
+                    error.message});
+        });
+});
+
+app.get('/api/events', (req, res) => {
+    // Logic to fetch all e-mails
+    let dateNow = new Date().toISOString();
+    supabase
+        .from('events')
+        .select('*')
+        .rangeGte('endTime', dateNow)
+        .then(response => {
+            console.log(response);
+            // Assuming you want to send this data back to the client
+            res.status(200).json(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({message: 'Error reading from Database: ' +
+                    error.message});
+        });
 });
 
 //===================-Listening port-=======================
